5874d3bd7ba78ef75f9d95cfd311ef2c080fe583
Valentin <v.stauber@parityqc.com>
2024-05-27 14:57:01 +0200

    formating, requirements

--- d3pm/images/diffusion_categorical.py
+++ d3pm/images/diffusion_categorical.py
@@ -15,12 +15,13 @@
 
 """Diffusion for discrete state spaces."""
 
-from . import utils
 import jax
-from jax import lax
 import jax.numpy as jnp
 import numpy as onp
 import scipy.special
+from jax import lax
+
+from . import utils
 
 
 def make_diffusion(hps, num_bits):
--- d3pm/images/main_test.py
+++ d3pm/images/main_test.py
@@ -17,50 +17,48 @@
 
 import random
 
-from absl.testing import absltest
-from flax import linen as nn
 import jax
 import numpy as onp
+from absl.testing import absltest
+from flax import linen as nn
 
-from d3pm.images import main
-from d3pm.images import main_test_config
-from d3pm.images import model
-from d3pm.images import utils
+from d3pm.images import main, main_test_config, model, utils
 
 
 class MainTest(absltest.TestCase):
 
-  def test_small_training_job(self):
-    experiment_dir = self.create_tempdir().full_path
-    work_unit_dir = self.create_tempdir().full_path
+    def test_small_training_job(self):
+        experiment_dir = self.create_tempdir().full_path
+        work_unit_dir = self.create_tempdir().full_path
 
-    # Disable compiler optimizations for faster compile time.
-    jax.config.update('jax_disable_most_optimizations', True)
+        # Disable compiler optimizations for faster compile time.
+        jax.config.update("jax_disable_most_optimizations", True)
 
-    # Seed the random number generators.
-    random.seed(0)
-    onp.random.seed(0)
-    rng = utils.RngGen(jax.random.PRNGKey(0))
+        # Seed the random number generators.
+        random.seed(0)
+        onp.random.seed(0)
+        rng = utils.RngGen(jax.random.PRNGKey(0))
 
-    # Construct a test config with a small number of steps.
-    config = main_test_config.get_config()
+        # Construct a test config with a small number of steps.
+        config = main_test_config.get_config()
 
-    # Patch normalization so that we don't try to apply GroupNorm with more
-    # groups than test channels.
-    orig_normalize = model.Normalize
-    try:
-      model.Normalize = nn.LayerNorm
+        # Patch normalization so that we don't try to apply GroupNorm with more
+        # groups than test channels.
+        orig_normalize = model.Normalize
+        try:
+            model.Normalize = nn.LayerNorm
 
-      # Make sure we can train without any exceptions.
-      main.run_train(
-          config=config,
-          experiment_dir=experiment_dir,
-          work_unit_dir=work_unit_dir,
-          rng=rng)
+            # Make sure we can train without any exceptions.
+            main.run_train(
+                config=config,
+                experiment_dir=experiment_dir,
+                work_unit_dir=work_unit_dir,
+                rng=rng,
+            )
 
-    finally:
-      model.Normalize = orig_normalize
+        finally:
+            model.Normalize = orig_normalize
 
 
-if __name__ == '__main__':
-  absltest.main()
+if __name__ == "__main__":
+    absltest.main()
